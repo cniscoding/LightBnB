c517be4b06e452f8aecbeddd72b6a5cf5ad0e4f8
cniscoding <asdfa.com>
2023-03-09 22:11:39 -0800

    refactored getAllReservations in database.js

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -89,7 +89,28 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  // return getAllProperties(null, 2);
+  const queryString =`
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`
+
+  return pool
+  .query(queryString, [guest_id, limit])
+  .then((result) => {
+    // console.log('addUser', result.rows)
+    return result.rows
+  })
+  .catch((err) => {
+    // console.log(err.message);
+    return null
+  });
+
 }
 exports.getAllReservations = getAllReservations;
 
